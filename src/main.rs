46f544bcee58151bfd6cc4949dbb9e0505451a10
Reinhardt <reinhardt.cgr@gmail.com>
2022-03-13 21:32:48 +1300

    was able to get the environment variables loaded and parsing the json string now

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,33 @@ use std::fs::File;
 //use std::io::{self, BufReader};
 use std::io::prelude::*;
 use std::path::Path;
+use dotenv::dotenv;
+use std::collections::HashMap;
+use serde_json::{Result, Value};
+
+// // x01: This doesn't seem to work right now >> will try alternative
+//fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> where P: AsRef<Path>, {
+    //let file  = File::open(filename)?;
+    //Ok(io::BufReader::new(file).lines())
+//}
 
 fn main() {
+    dotenv().ok();
+    //for (key, value) in env::vars() {
+        //println!("{}: {}", key, value);
+    //}
+    let token_key = "BEARER_TOKEN";
+    let token = dotenv::var(token_key).unwrap();
+    println!("{}: {}",  token_key, token);
+
+    let notion_key = "NOTION_VERSION";
+    let notion_version = dotenv::var(notion_key).unwrap();
+    println!("{}: {}",  notion_key, notion_version);
+    
+    let database_key = "DATABASE_ID";
+    let database_id = dotenv::var(database_key).unwrap();
+    println!("{}: {}",  database_key, database_id);
+
     //println!("Hello, world!");
     let args: Vec<String> = env::args().collect();
     let command_path = &args[0];
@@ -35,13 +60,23 @@ fn main() {
     let mut s = String::new();
     match file.read_to_string(&mut s) {
         Err(why) => panic!("Could not read {}: {}", display, why),
-        Ok(_) => println!("{} contains: \n{}", display, s),
+        Ok(_) => transform_pixels_for_notion(s),
+        //Ok(_) => println!("Was able to read file"),
+        //Ok(_) => println!("{} contains: \n{}", display, s),
     };
 
 }
 
-// // x01: This doesn't seem to work right now >> will try alternative
-//fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> where P: AsRef<Path>, {
-    //let file  = File::open(filename)?;
-    //Ok(io::BufReader::new(file).lines())
-//}
+fn transform_pixels_for_notion(json_string: String) -> Result<()> {
+    println!("Read file now parsing...");
+    let my_json: Vec<HashMap<String, Value>> = serde_json::from_str(&json_string)?;
+    for item in my_json.iter() {
+        //println!("> {}", item);
+        //println!("> {}", item.keys());
+        println!("> {:?}", item);
+        println!("> {:?}", item.keys());
+    }
+    //println!("{:?}", my_json);
+
+    Ok(())
+}
