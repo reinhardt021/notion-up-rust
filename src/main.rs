36d521197a17ec7834ffe8e51431b369505b84e7
Reinhardt <reinhardt.cgr@gmail.com>
2022-03-13 16:56:35 +1300

    have the reading command line parameters working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let my_path = &args[0];
+    println!("My path is {}.", my_path);
+    let param_count = args.len() - 1;
+    println!("I got {:?} arguments: {:?}.", param_count, &args[1..]);
+
+    //println!("Hello, world!");
 }
